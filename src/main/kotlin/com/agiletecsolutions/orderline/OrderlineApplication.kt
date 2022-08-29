bb299daa08d05d6f2d707939063828770cea7742
thankGodForLife <28709874+xodiumluma@users.noreply.github.com>
2022-08-30 01:41:53 +1000

    Initial commit

---
+++ src/main/kotlin/com/agiletecsolutions/orderline/OrderlineApplication.kt
@@ -0,0 +1,11 @@
+package com.agiletecsolutions.orderline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderlineApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderlineApplication>(*args)
+}
---
+++ src/test/kotlin/com/agiletecsolutions/orderline/OrderlineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.agiletecsolutions.orderline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderlineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
